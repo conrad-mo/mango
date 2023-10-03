47b413b9e33d4328a481ceb282ef62dd096c2935
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2023-10-03 15:47:26 -0400

    Fix more bugs but still a lot to iron out before continuing anything

--- src/types.rs
+++ src/types.rs
@@ -23,19 +23,23 @@ pub(crate) async fn decompress_tgz(name: String) {
     let mut a = Archive::new(dec);
     for entry in a.entries().unwrap() {
         let mut entry = entry.unwrap();
-        // Get the path of the entry
         let mut entry_path = PathBuf::new();
         entry_path.push("node_modules/");
         entry_path.push(&name);
-        entry_path.push(entry.path().unwrap().strip_prefix("package").unwrap());
+        match entry.path().unwrap().strip_prefix("package") {
+            Ok(subpath) => {
+                entry_path.push(subpath);
+                if let Some(parent) = entry_path.parent() {
+                    std::fs::create_dir_all(parent).unwrap();
+                }
+                entry.unpack(&entry_path).unwrap();
+            }
+            Err(error) => {
+                println!("Failed to strip prefix: {:?}", error);
 
-        // Create the necessary parent directories
-        if let Some(parent) = entry_path.parent() {
-            std::fs::create_dir_all(parent).unwrap();
+
+            }
         }
-
-        // Extract the entry to the specified path
-        entry.unpack(&entry_path).unwrap();
     }
     let _ = fs::remove_file(format!("node_modules/{}.tgz", name));
     println!("Done unzipping");
